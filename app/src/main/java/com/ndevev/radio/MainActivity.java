24caa3a866452cdd03a29b6ed65e71040e534e45
ndevev <nevi.coyote@freemail.hu>
2016-10-16 10:23:33 +0200

    starting steps

--- app/src/main/java/com/ndevev/radio/MainActivity.java
+++ app/src/main/java/com/ndevev/radio/MainActivity.java
@@ -12,6 +12,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        startButton = findViewById(R.id.test_button);
+        startButton = (Button) findViewById(R.id.test_button);
     }
 }
